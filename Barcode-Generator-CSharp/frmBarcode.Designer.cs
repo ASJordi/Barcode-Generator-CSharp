7331d2f75a2b3d33f68a74f693a11f360018c57a
Jordi Ayala <jord7as@gmail.com>
2022-02-14 16:32:51 -0600

    Add files - Project

--- Barcode-Generator-CSharp/frmBarcode.Designer.cs
+++ Barcode-Generator-CSharp/frmBarcode.Designer.cs
@@ -29,20 +29,176 @@ namespace Barcode_Generator_CSharp
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBarcode));
+            this.panelPrincipal = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.cmTypesBarcode = new System.Windows.Forms.ComboBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.pbCode = new System.Windows.Forms.PictureBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.panelPrincipal.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCode)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panelPrincipal
+            // 
+            this.panelPrincipal.BackColor = System.Drawing.Color.White;
+            this.panelPrincipal.Controls.Add(this.btnSave);
+            this.panelPrincipal.Controls.Add(this.pbCode);
+            this.panelPrincipal.Controls.Add(this.btnGenerate);
+            this.panelPrincipal.Controls.Add(this.cmTypesBarcode);
+            this.panelPrincipal.Controls.Add(this.txtCode);
+            this.panelPrincipal.Controls.Add(this.txtTitle);
+            this.panelPrincipal.Controls.Add(this.label3);
+            this.panelPrincipal.Controls.Add(this.label2);
+            this.panelPrincipal.Controls.Add(this.label1);
+            this.panelPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.panelPrincipal.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panelPrincipal.Name = "panelPrincipal";
+            this.panelPrincipal.Padding = new System.Windows.Forms.Padding(40, 39, 40, 39);
+            this.panelPrincipal.Size = new System.Drawing.Size(326, 422);
+            this.panelPrincipal.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Code:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Type:";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTitle.Location = new System.Drawing.Point(12, 33);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(300, 23);
+            this.txtTitle.TabIndex = 3;
+            // 
+            // txtCode
+            // 
+            this.txtCode.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCode.Location = new System.Drawing.Point(9, 92);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(300, 23);
+            this.txtCode.TabIndex = 4;
+            // 
+            // cmTypesBarcode
+            // 
+            this.cmTypesBarcode.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cmTypesBarcode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmTypesBarcode.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmTypesBarcode.FormattingEnabled = true;
+            this.cmTypesBarcode.ItemHeight = 17;
+            this.cmTypesBarcode.Location = new System.Drawing.Point(9, 148);
+            this.cmTypesBarcode.Name = "cmTypesBarcode";
+            this.cmTypesBarcode.Size = new System.Drawing.Size(300, 25);
+            this.cmTypesBarcode.TabIndex = 5;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btnGenerate.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGenerate.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnGenerate.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.ForeColor = System.Drawing.Color.White;
+            this.btnGenerate.Location = new System.Drawing.Point(114, 189);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(94, 33);
+            this.btnGenerate.TabIndex = 6;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = false;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // pbCode
+            // 
+            this.pbCode.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbCode.Location = new System.Drawing.Point(12, 232);
+            this.pbCode.MaximumSize = new System.Drawing.Size(300, 125);
+            this.pbCode.MinimumSize = new System.Drawing.Size(300, 125);
+            this.pbCode.Name = "pbCode";
+            this.pbCode.Size = new System.Drawing.Size(300, 125);
+            this.pbCode.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbCode.TabIndex = 7;
+            this.pbCode.TabStop = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.btnSave.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnSave.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(114, 369);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 33);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // frmBarcode
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(326, 422);
+            this.Controls.Add(this.panelPrincipal);
+            this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximumSize = new System.Drawing.Size(342, 461);
+            this.MinimumSize = new System.Drawing.Size(342, 461);
             this.Name = "frmBarcode";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Barcode Generator ";
+            this.Load += new System.EventHandler(this.frmBarcode_Load);
+            this.panelPrincipal.ResumeLayout(false);
+            this.panelPrincipal.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCode)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelPrincipal;
+        private System.Windows.Forms.ComboBox cmTypesBarcode;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.PictureBox pbCode;
     }
 }
 
--- Barcode-Generator-CSharp/frmBarcode.cs
+++ Barcode-Generator-CSharp/frmBarcode.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Imaging;
 using System.Windows.Forms;
+using BarcodeLib;
 
 namespace Barcode_Generator_CSharp
 {
@@ -15,6 +11,113 @@ namespace Barcode_Generator_CSharp
         public frmBarcode()
         {
             InitializeComponent();
+        }
+
+        public class OpcionCombo
+        {
+            public int Valor { get; set; }
+            public string Texto { get; set; }
+        }
+
+        private void frmBarcode_Load(object sender, EventArgs e)
+        {
+            LoadBarcodeTypes();
+        }
+
+        private void LoadBarcodeTypes()
+        {
+            //Load barcode types
+            int index = 0;
+            foreach (var name in Enum.GetNames(typeof(TYPE)))
+            {
+                cmTypesBarcode.Items.Add(new OpcionCombo() { Valor = index, Texto = name });
+                index++;
+            }
+
+            cmTypesBarcode.DisplayMember = "Texto";
+            cmTypesBarcode.ValueMember = "Valor";
+            cmTypesBarcode.SelectedIndex = 31;
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Image CodeImage;
+                int index = (cmTypesBarcode.SelectedItem as OpcionCombo).Valor;
+                TYPE codeType = (TYPE)index;
+
+                Barcode code = new Barcode();
+                code.IncludeLabel = true;
+                code.LabelPosition = LabelPositions.BOTTOMCENTER;
+
+                //set code of txtCode
+                CodeImage = code.Encode(codeType, txtCode.Text.Trim(), Color.Black, Color.White, 300, 100);
+
+                //set txtTitle as extra in image
+                Bitmap imageTitle = ConvertTextToImage(txtTitle.Text.Trim(), 300, Color.White);
+                int NewHeightImage = CodeImage.Height + imageTitle.Height;
+                Bitmap newImage = new Bitmap(300, NewHeightImage);
+                Graphics draw = Graphics.FromImage(newImage);
+                draw.DrawImage(imageTitle, new Point(0, 0));
+                draw.DrawImage(CodeImage, new Point(0, imageTitle.Height));
+
+                //set image in PictureBox
+                pbCode.BackgroundImage = newImage;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Not supported operation" + Environment.NewLine + "Please review the text and code input", "Barcode Generator", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Image codeImage = pbCode.BackgroundImage.Clone() as Image;
+
+            SaveFileDialog windowFile = new SaveFileDialog();
+            windowFile.FileName = string.Format("{0}.png", txtCode.Text.Trim());
+            windowFile.Filter = "Imagen |*.png";
+
+            if (windowFile.ShowDialog() == DialogResult.OK)
+            {
+                codeImage.Save(windowFile.FileName, ImageFormat.Png);
+                MessageBox.Show("Barcode generated", "Barcode Generator", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        public static Bitmap ConvertTextToImage(string texto, int _width, Color color)
+        {
+            //Create object Bitmap
+            Bitmap objBitmap = new Bitmap(1, 1);
+            int Width = 0;
+            int Height = 0;
+            //format the font (font type, size)
+            Font objFont = new Font("Arial", 16, FontStyle.Bold, GraphicsUnit.Pixel);
+
+            //Create Graphics object from Bitmap obejct
+            Graphics objGraphics = Graphics.FromImage(objBitmap);
+
+            //Set the size according to the length of the text
+            Width = _width;
+            Height = (int)objGraphics.MeasureString(texto, objFont).Height + 5;
+            objBitmap = new Bitmap(objBitmap, new Size(Width, Height));
+
+            objGraphics = Graphics.FromImage(objBitmap);
+
+            objGraphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            objGraphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+            objGraphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+            objGraphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+
+            StringFormat drawFormat = new StringFormat();
+            objGraphics.Clear(color);
+
+            drawFormat.Alignment = StringAlignment.Center;
+            objGraphics.DrawString(texto, objFont, new SolidBrush(Color.Black), new RectangleF(0, (objBitmap.Height / 2) - (objBitmap.Height - 10), objBitmap.Width, objBitmap.Height), drawFormat);
+            objGraphics.Flush();
+
+            return objBitmap;
         }
     }
 }
